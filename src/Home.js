5ecf99e3cd7c7b1d5374d5b987e53bfa36201255
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-04-16 18:25:38 -0300

    Conteúdo Home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, TouchableHighlight } from 'react-native';
 
 export default class Home extends Component {
 
@@ -8,20 +8,69 @@ export default class Home extends Component {
     header: null
   }
 
+  constructor(props){
+    super(props);
+    this.state = {};
+
+    this.cadastrar = this.cadastrar.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  cadastrar() {
+    this.props.navigation.navigate('Cadastro');
+  }
+
+  login() {
+    this.props.navigation.navigate('Login');
+  }
+
   render() {
     return(
       <ImageBackground source={require('../assets/images/fundo.jpg')} style={styles.bg} >
-
+        <View style={styles.container}>
+          <Text style={styles.title}>Fluxo de Caixa</Text>
+          <View style={styles.buttonArea}>
+            <TouchableHighlight underlayColor="#cccccc" onPress={this.cadastrar} style={styles.button}>
+              <Text style={styles.btnText}> Cadastrar </Text>
+            </TouchableHighlight>
+              
+            <TouchableHighlight underlayColor="#cccccc" onPress={this.login} style={styles.button}>
+              <Text style={styles.btnText}> Login </Text>
+            </TouchableHighlight>
+          </View>
+        </View>
       </ImageBackground>
     );
   }
 }
 
 //Estilos
-
 const styles = StyleSheet.create({
   bg: {
     flex: 1,
     width: null
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 30,
+    backgroundColor: 'transparent',
+  },
+  buttonArea: {
+    marginTop: 30
+  },
+  button: {
+    backgroundColor: '#bfb300',
+    margin: 10,
+    height: 40,
+    width: 200,
+    justifyContent: 'center'
+  },
+  btnText: {
+    color: '#ffffff',
+    textAlign: 'center'
   }
 });
