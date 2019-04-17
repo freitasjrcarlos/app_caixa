f61511ce0c2c63cd1c44a066d10885593f270f2f
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-04-17 09:31:13 -0300

    Add estrutura da tela interna

--- src/Interna.js
+++ src/Interna.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 
 export default class Interna extends Component {
 
@@ -8,10 +8,40 @@ export default class Interna extends Component {
     header: null
   }
 
+  constructor(props){
+    super(props);
+    this.state = {
+      saldo: 0,
+      historico: [],
+    };
+
+    this.addReceita = this.addReceita.bind(this);
+    this.addDespesa = this.addDespesa.bind(this);
+  }
+
+  addReceita() {
+
+  }
+
+  addDespesa() {
+
+  }
+
   render() {
     return(
       <View style={styles.container}>
-        <Text> ... </Text>
+        <View style={styles.saldoArea}>
+          <Text style={styles.saldo}>Saldo: R$ {this.state.saldo}</Text>
+        </View>
+        <FlatList
+          style={styles.historico}
+          data={this.state.historico}
+          renderItem={(item)=> <HistoricoItem data={item} />}
+        />
+        <View style={styles.botoesArea}>
+          <Button title="+ Receita" onPress={this.addReceita} />
+          <Button title="+ Despesa" onPress={this.addDespesa} />
+        </View>
       </View>
     );
   }
@@ -20,6 +50,26 @@ export default class Interna extends Component {
 //Estilos
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     marginTop: 20,
+  },
+  saldoArea: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: '#dddddd',
+  },
+  saldo: {
+    textAlign: 'center',
+    fontSize: 25,
+  },
+  historico: {
+    flex: 1,
+  },
+  botoesArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#dddddd',
   }
 });
